2e4fff27bcc3a3701547a8bf88ab4db23b927202
Hao Fan <haofan1126@gmail.com>
2016-09-11 09:53:35 -0400

    Update FIS.py

--- FIS.py
+++ FIS.py
@@ -1,5 +1,5 @@
-def getMembership(degree,member,x):
-    a,b,c,d = degree[member]
+def getMembership(mf,degree,x):
+    a,b,c,d = mf[degree]
     if x<=a or x>=d:
         return 0.0
     else:
@@ -19,16 +19,18 @@ def FIS(get_speed,get_dist):
     mf_speed = {'LOW': (0,0,10,20),'MEDIUM':(15,25,35,45),'HIGH':(40,50,60,60)}
     mf_closeness = {'LOW':(1.0,1.5,2.0,2.0) ,'MEDIUM':(0.5,0.75,1.0,1.25),'HIGH':(0,0,0.25,0.75)}
     mfs = [[mf_speed,get_speed],[mf_closeness,get_dist]]
+    
     rulebase = [['HIGH',-10],['MEDIUM',5],['LOW',10]]
 
-    sumx,sumfx = 0,0
+    sum_w,sum_v = 0,0
+    
     for j in xrange(len(mfs)):
         for i in rulebase:
-            value = getMembership(mfs[j][0],i[0],mfs[j][1])
+            weight = getMembership(mfs[j][0],i[0],mfs[j][1])
+            sum_w += weight
+            sum_v += weight * i[-1]
 
-            sumx += value
-            sumfx += value * i[-1]
-    return sumfx / sumx
+    return sum_v / sum_w
 
 get_speed = int(raw_input("Your Speend? (0-60 mph)"))
 get_dist = float(raw_input("Your Distance to Target? (0-2.0 miles)"))
